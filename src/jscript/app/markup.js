12a3987d105386ddf5a1417aabb2eb113d9d24d8
Dmitrij Prokhorenko <dmitrij.prokhorenko@gmail.com>
2020-02-23 23:45:20 +0200

    in process

---
+++ src/jscript/app/markup.js
@@ -0,0 +1,32 @@
+import resultCountry from '../templates/resultCountry.hbs';
+import resultCountryList from '../templates/resultCountryList.hbs';
+import fetchCountries from './fetchCountries';
+import PNotify from 'node_modules/pnotify/dist/es/PNotify.js';
+
+const jsResult = document.querySelector('.js-result');
+const input = document.getElementById('search-input');
+const countryList = document.querySelector('.list-of-countries');
+
+function markup(e) {
+  const textInInput = e.target.value;
+
+  fetchCountries(textInInput)
+    .then(res => res.json())
+    .then(data => {
+      jsResult.innerHTML = '';
+      countryList.innerHTML = '';
+      if (data.length >= 2 && data.length <= 10) {
+        countryList.innerHTML = data
+          .map(item => resultCountryList(item))
+          .join('');
+      } else if (data.length === 1) {
+        jsResult.innerHTML = resultCountry(...data);
+      } else {
+        return PNotify.error({
+          text: 'Too many matches found. Please enter a more specific query!',
+        });
+      }
+    });
+}
+
+input.addEventListener('input', markup);
