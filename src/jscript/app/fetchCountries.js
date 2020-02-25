16d0ee77e01c957b48651bc979a7e8f05175d09e
Dmitrij Prokhorenko <dmitrij.prokhorenko@gmail.com>
2020-02-25 22:39:44 +0200

    all done

--- src/jscript/app/fetchCountries.js
+++ src/jscript/app/fetchCountries.js
@@ -1,3 +1,25 @@
+function finder(query, item) {
+  const array = query.toLowerCase().split('');
+  const normalizedItem = item.toLowerCase();
+  let resultArr = [];
+
+  for (const itm of array) {
+    if (normalizedItem.includes(itm)) {
+      resultArr.push(1);
+    }
+  }
+
+  if (resultArr.length === array.length) {
+    return true;
+  }
+
+  return false;
+}
+
 export default function fetchCountries(searchQuery) {
-  return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`);
+  return fetch(
+    `https://restcountries.eu/rest/v2/name/${searchQuery}?fields=name;capital;population;languages;flag`,
+  )
+    .then(res => res.json())
+    .then(data => data.filter(item => finder(searchQuery, item.name)));
 }
--- src/jscript/app/markup.js
+++ src/jscript/app/markup.js
@@ -2,32 +2,46 @@ import resultCountry from '../templates/resultCountry.hbs';
 import resultCountryList from '../templates/resultCountryList.hbs';
 import fetchCountries from './fetchCountries';
 import PNotify from 'pnotify/dist/es/PNotify';
+
 const debounce = require('lodash.debounce');
 
-const jsResult = document.querySelector('.js-result');
-const input = document.getElementById('search-input');
-const countryList = document.querySelector('.list-of-countries');
+const markupRef = {
+  jsResult: document.querySelector('.js-result'),
+  input: document.getElementById('search-input'),
+  countryList: document.querySelector('.list-of-countries'),
+};
 
 function markup(e) {
   const textInInput = e.target.value;
 
   fetchCountries(textInInput)
-    .then(res => res.json())
     .then(data => {
-      jsResult.innerHTML = '';
-      countryList.innerHTML = '';
-      if (data.length >= 2 && data.length <= 10) {
-        countryList.innerHTML = data
+      markupRef.jsResult.innerHTML = '';
+      markupRef.countryList.innerHTML = '';
+
+      if (data.length > 1 && data.length <= 10) {
+        markupRef.countryList.innerHTML = data
           .map(item => resultCountryList(item))
           .join('');
-      } else if (data.length === 1) {
-        jsResult.innerHTML = resultCountry(...data);
-      } else {
-        return PNotify.error({
-          text: 'Too many matches found. Please enter a more specific query!',
-        });
+        return;
       }
+
+      if (data.length === 1) {
+        markupRef.jsResult.innerHTML = resultCountry(...data);
+        return;
+      }
+
+      return PNotify.error({
+        text: 'Too many matches found. Please enter a more specific query!',
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      markupRef.countryList.innerHTML = '';
+      return PNotify.error({
+        text: 'No text in input. Please, write some letters!',
+      });
     });
 }
 
-input.addEventListener('input', debounce(markup, 500));
+markupRef.input.addEventListener('input', debounce(markup, 500));
